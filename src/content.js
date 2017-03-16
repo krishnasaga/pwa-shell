afe3e94a7dc69478ce8dabcd9fb58feafc5585bd
krishnasagar.r@sonata-software.com <T12449@thomsonuk.dnsroot.biz>
2017-03-16 02:28:08 +0000

    add books

--- src/content.js
+++ src/content.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import Books from './Books/Books.js'
+import './content.scss'
 
 ReactDom.render(<Books/>,document.getElementById('content'));
